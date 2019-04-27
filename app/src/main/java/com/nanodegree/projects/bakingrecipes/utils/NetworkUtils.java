31a24acd36446be25dcb8a1c1a71676eb7457ed0
deepikanathan <dvarun83@gmail.com>
2019-04-26 23:00:05 -0500

    Network Utility methods

---
+++ app/src/main/java/com/nanodegree/projects/bakingrecipes/utils/NetworkUtils.java
@@ -0,0 +1,14 @@
+package com.nanodegree.projects.bakingrecipes.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+public class NetworkUtils {
+
+    public static boolean isNetworkAvailable(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+    }
+}
