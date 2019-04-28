012ad6269dedb137b26606b28319e82a769dcebf
deepikanathan <dvarun83@gmail.com>
2019-04-28 12:04:24 -0500

    Global Application

---
+++ app/src/main/java/com/nanodegree/projects/bakingrecipes/GlobalApplication.java
@@ -0,0 +1,44 @@
+package com.nanodegree.projects.bakingrecipes;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
+import com.nanodegree.projects.bakingrecipes.IdlingRecource.RecipesIdlingResource;
+
+/**
+ * Application to get App Context that is used in utility classes where context is not available.
+ * Reference - https://www.dev2qa.com/android-get-application-context-from-anywhere-example/
+ */
+public class GlobalApplication extends Application {
+
+    @Nullable
+    private RecipesIdlingResource recipesIdlingResource;
+
+    @VisibleForTesting
+    @NonNull
+    private IdlingResource initializeIdlingResource() {
+        if (recipesIdlingResource == null) {
+            recipesIdlingResource = new RecipesIdlingResource();
+        }
+        return recipesIdlingResource;
+    }
+
+    public GlobalApplication() {
+
+        if (BuildConfig.DEBUG) {
+            initializeIdlingResource();
+        }
+    }
+
+    public void setIdleState(boolean state) {
+        if (recipesIdlingResource != null)
+            recipesIdlingResource.setIdleState(state);
+    }
+
+    @Nullable
+    public RecipesIdlingResource getIdlingResource() {
+        return recipesIdlingResource;
+    }
+}
