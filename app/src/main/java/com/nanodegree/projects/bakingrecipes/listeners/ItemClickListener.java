7813c64a45f10f933da0abc5ab336011442d5378
deepikanathan <dvarun83@gmail.com>
2019-04-29 22:02:59 -0500

    item click listener

---
+++ app/src/main/java/com/nanodegree/projects/bakingrecipes/listeners/ItemClickListener.java
@@ -0,0 +1,9 @@
+package com.nanodegree.projects.bakingrecipes.listeners;
+
+
+public final class ItemClickListener {
+
+    public interface OnItemClickListener {
+        void onItemClick(int pos);
+    }
+}
---
+++ app/src/main/java/com/nanodegree/projects/bakingrecipes/listeners/OnRecipeClickListener.java
@@ -0,0 +1,7 @@
+package com.nanodegree.projects.bakingrecipes.listeners;
+
+import com.nanodegree.projects.bakingrecipes.models.Recipe;
+
+public interface OnRecipeClickListener {
+    void onRecipeSelected(Recipe recipe);
+}
