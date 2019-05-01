05e86b9c256bd5c93a615b1467d942d4afb74e1d
deepikanathan <dvarun83@gmail.com>
2019-04-30 22:10:08 -0500

    Step FragmentPagerAdapter

---
+++ app/src/main/java/com/nanodegree/projects/bakingrecipes/adapters/StepsFragmentPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.nanodegree.projects.bakingrecipes.adapters;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.nanodegree.projects.bakingrecipes.R;
+import com.nanodegree.projects.bakingrecipes.models.Step;
+import com.nanodegree.projects.bakingrecipes.ui.fragments.RecipeStepDetailFragment;
+
+import java.util.List;
+
+public class StepsFragmentPagerAdapter extends FragmentPagerAdapter {
+    private Context ctx;
+    private List<Step> recipeSteps;
+
+    public StepsFragmentPagerAdapter(Context context, FragmentManager fragmentManager, List<Step> steps) {
+        super(fragmentManager);
+        this.ctx = context;
+        this.recipeSteps = steps;
+    }
+
+    @Override
+    public int getCount() {
+        return recipeSteps.size();
+    }
+
+   @Override
+    public Fragment getItem(int position) {
+        Bundle arguments = new Bundle();
+        //  pass the Recipe Step Number and Step Description to the succeeding Step
+        arguments.putParcelable(RecipeStepDetailFragment.STEP_KEY, recipeSteps.get(position));
+        RecipeStepDetailFragment fragment = new RecipeStepDetailFragment();
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return String.format(ctx.getString(R.string.step_number), position + 1);
+    }
+
+
+
+
+}
